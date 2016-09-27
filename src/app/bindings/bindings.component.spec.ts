01ee4898542d364cb959008fb069adb87dc22f90
¨udr013¨ <udr013@gmail.com>
2016-09-27 15:40:49 +0200

    cleanup and changed prefix

---
+++ src/app/bindings/bindings.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BindingsComponent } from './bindings.component';
+
+describe('Component: Bindings', () => {
+  it('should create an instance', () => {
+    let component = new BindingsComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/bindings/bindings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ud-bindings',
+  templateUrl: './bindings.component.html',
+  styles: []
+})
+export class BindingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
